51aa2364627041bfb71f4cfe7069ff946d9c2b56
Viki Harrod <victoria.harrod@gmail.com>
2014-11-12 21:33:33 -0700

    add contact and fix broken tests from adding company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Company, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) { {name: "Big Corp, Inc."} }
+  let(:company) { Company.create(valid_attributes) }
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    company = Company.create(name: nil)
+    expect(company).not_to be_valid
+  end
+
+  it 'has an array of phone numbers' do
+    expect(company.phone_numbers).to eq([])
+  end
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, :type => :model do
+  let(:person) { Person.create(first_name: 'Jimbob', last_name: 'Billy')}
   let(:phone_number) do
-    PhoneNumber.new(number: '1112223333', person_id: 1)
+    PhoneNumber.new(number: '1112223333', contact_id: person.id, contact_type: 'Person')
   end
 
   it 'is_valid' do
@@ -15,11 +16,11 @@ RSpec.describe PhoneNumber, :type => :model do
   end
 
   it 'must have a reference to a person' do
-    phone_number.person_id = nil
+    phone_number.contact_id = nil
     expect(phone_number).not_to be_valid
   end
 
-  it 'is associated with a person' do
-    expect(phone_number).to respond_to(:person)
+  it 'is associated with a contact' do
+    expect(phone_number).to respond_to(:contact)
   end
 end
